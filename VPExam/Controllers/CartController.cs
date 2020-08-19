6277f1cb60836ad3ee7f55fa6cd5a2d42d3a2d03
dungnguyentien@gmail.com <dungnguyentien@gmail.com>
2020-08-19 20:56:43 +0700

    commit VpExam

---
+++ VPExam/Controllers/CartController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VPExam.Models;
+using VPExam.Repository;
+
+namespace VPExam.Controllers
+{
+    public class CartController : Controller
+    {
+        private CartRepository cartRepository = new CartRepository();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            ModelState.Clear();
+            int? pageNumber = 0;
+            var model = cartRepository.GetCarts(pageNumber);
+            return View(model);
+        }
+        /// <summary>
+        /// Add item in to card
+        /// </summary>
+        /// <param name="ItemId"></param>
+        /// <param name="ItemName"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public int AddItem(int ItemId, string ItemName)
+        {
+            Cart cart = new Cart();
+            int count = GetTotal();
+            cart.ItemId = ItemId;
+            cart.ItemName = ItemName + " in cart " + count++;
+            cartRepository.AddItem(cart);
+            return cart.Id;
+        }
+        /// <summary>
+        /// Get All item
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult IteamList(int? pageNumber)
+        {
+            ModelState.Clear();
+            var model = cartRepository.GetCarts(pageNumber);
+            return PartialView("~/Views/Cart/CartList.cshtml", model);
+        }
+        /// <summary>
+        /// Delete item
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult RemoveItem(int Id)
+        {
+            int? pageNumber = 0;
+            ModelState.Clear();
+            cartRepository.Remove(Id);
+            var redirectUrl = new UrlHelper(Request.RequestContext).Action("Index", "Cart");
+            var model = cartRepository.GetCarts(pageNumber);
+            return PartialView("~/Views/Cart/CartList.cshtml", model);
+        }
+        /// <summary>
+        /// Count item
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public int GetTotal()
+        {
+            var count = cartRepository.GetTotal();
+            return count;
+        }
+    }
+}
---
+++ VPExam/Controllers/ItemController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VPExam.Repository;
+
+namespace VPExam.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ItemRepository productRepository;
+        public ActionResult Index()
+        {
+            int? pageNumber=0;
+            productRepository = new ItemRepository();
+            var model = productRepository.GetItems(pageNumber);
+            return View(model);
+        }
+        /// <summary>
+        /// Get All Item
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult ItemList(int? pageNumber)
+        {
+            productRepository = new ItemRepository();
+            var model = productRepository.GetItems(pageNumber);
+            return PartialView("~/Views/Item/ItemList.cshtml", model);
+        }         
+    }
+}
